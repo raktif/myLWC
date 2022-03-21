e6a53784f56514a98b99219473d8274d692cb3da
Lucas Fernandes <fernandes_lucas@hotmail.com>
2022-03-21 13:54:28 -0300

    remove comments

--- force-app/main/default/lwc/menuOrder/menuOrder.js
+++ force-app/main/default/lwc/menuOrder/menuOrder.js
@@ -6,10 +6,10 @@ export default class MenuOrder extends LightningElement {
     @track accountId;
     @track accountName;
     @track showValues = false;
-    @api totalAmount;
+    @track totalAmount;
     @track dishList=[];
-    @track componentIds = []; //??
-    @track componentId; //??
+    @track componentIds = [];
+    @track componentId;
     @track currentDish;
 
     handleAccountName(event) {
@@ -32,16 +32,11 @@ export default class MenuOrder extends LightningElement {
     addMenuOrderDetail() {
         this.addComponentId();
         this.dishList.push(this.componentId);
-        console.log('dishList: '+this.dishList.length);
     }
 
     removeRow(event) {
         const dishIndex = event.detail;
-        console.log('dishListJSON: '+JSON.stringify(this.dishList));
-        console.log('dishIndex: '+dishIndex);
-        console.log('componentIds[dishIndex]: '+this.componentIds[dishIndex]);
         this.dishList.splice(this.componentIds[dishIndex], 1);
-        console.log('new dishListJSON: '+JSON.stringify(this.dishList));
     }
 
        addComponentId() {
@@ -50,7 +45,6 @@ export default class MenuOrder extends LightningElement {
         } else {
             this.componentId++;
         }
-
         this.componentIds.push(this.componentId);
     }
 }
