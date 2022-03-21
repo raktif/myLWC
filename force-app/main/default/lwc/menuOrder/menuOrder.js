f5f4aa00187575a0c427806adc63ad5008a004ce
Lucas Fernandes <fernandes_lucas@hotmail.com>
2022-03-21 14:36:44 -0300

    another test

---
+++ force-app/main/default/lwc/menuOrder/menuOrder.js
@@ -0,0 +1,49 @@
+import { LightningElement, track, api } from 'lwc';
+import getLookupAccount from '@salesforce/apex/SearchController.searchAccounts';
+
+export default class MenuOrder extends LightningElement {
+    @track searchData;
+    @track accountId;
+    @track accountName;
+    @track showValues = false;
+    @track totalAmount = 0;
+    @track dishList=[];
+    @track componentIds = [];
+    @track componentId;
+    @track currentDish;
+
+    handleAccountName(event) {
+        getLookupAccount({accName: event.detail.value})
+            .then(result => {
+                this.searchData = result;
+                if (this.searchData != null) {
+                    this.showValues = true;
+                }
+            })
+    }
+
+    selectAccount(event) {
+        this.accountId = event.target.dataset.value;
+        this.accountName = event.target.dataset.label;
+        this.showValues = false;
+    }
+
+    addMenuOrderDetail() {
+        this.addComponentId();
+        this.dishList.push(this.componentId);
+    }
+
+    removeRow(event) {
+        const dishIndex = event.detail;
+        this.dishList.splice(this.componentIds[dishIndex], 1);
+    }
+
+       addComponentId() {
+        if (this.componentIds.length == 0) {
+            this.componentId = 0;
+        } else {
+            this.componentId++;
+        }
+        this.componentIds.push(this.componentId);
+    }
+}
---
+++ force-app/main/default/lwc/menuOrderDetails/menuOrderDetails.js
@@ -0,0 +1,56 @@
+import { LightningElement, track, api } from 'lwc';
+import getDishPrice from '@salesforce/apex/MenuCalculations.dishPrice';
+import subtotalCalc from '@salesforce/apex/MenuCalculations.subtotal';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class MenuOrderDetails extends LightningElement {
+
+    @track selectedDish;
+    @track selectedCuisine;
+    @track price;
+    @track quantity;
+    @track subtotal;
+    @api index;
+
+    getPrice(event) {
+        if (this.quantity != 1) {
+            this.quantity = 1;
+        }
+        this.selectedDish = event.detail.value;
+        getDishPrice({dishName: this.selectedDish})
+            .then (result => {
+                this.price = result;
+                this.calculateSubTotal(this.quantity, this.price);
+            })
+    }
+    
+    changeSubTotal(event) {
+        this.quantity = event.detail.value;
+
+        if (this.quantity < 1 || this.quantity == null) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Incorrect value.',
+                message: 'Quantity cannot be lower than 1.',
+                variant: 'error'
+            }));
+        } else {
+            this.calculateSubTotal();
+        }
+    }
+
+    calculateSubTotal() {
+        subtotalCalc({quantity: this.quantity, price: this.price})
+            .then (result => {
+                this.subtotal = 'US$'+result;
+            })
+    }
+
+    removeDish() {
+        const custEvent = new CustomEvent (
+            'removedish', {
+                detail: this.index
+            }
+        );
+        this.dispatchEvent(custEvent);
+    }
+}
